71f2ef0c6cb166876e7f545fe67fe239b8aefff2
NachHR <120605029+NachHR@users.noreply.github.com>
2026-09-16 12:12:55 +0200

    chore(v8.4.2): update service worker cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='filipinas-v8-4-1';
+const CACHE='filipinas-v8-4-2';
 const APP_SHELL=['./','./index.html','./style.css','./today.css','./data.js','./photos.js','./script.js','./today.js','./manifest.json','./icon-192.png','./icon-512.png'];
 const LOCAL_IMAGES=['./images/airport.webp','./images/bukidnon.webp','./images/camiguin.webp','./images/cdo.webp','./images/cebu.webp','./images/ferry.webp','./images/fort-san-pedro.webp','./images/guiob.webp','./images/iligan.webp','./images/kawasan.webp','./images/mantigue.webp','./images/oslob.webp','./images/santo-nino.webp','./images/sirao.webp','./images/taoist-temple.webp','./images/temple-of-leah.webp','./images/tuasan.webp'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll([...APP_SHELL,...LOCAL_IMAGES]))));
