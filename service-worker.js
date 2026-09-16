7a90c313df0f073619bb56799e2b8864e4126afd
NachHR <120605029+NachHR@users.noreply.github.com>
2026-09-16 06:49:28 +0200

    chore(v8.4): update service worker cache and app shell

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='filipinas-v8-3-0';
-const APP_SHELL=['./','./index.html','./style.css','./data.js','./photos.js','./script.js','./manifest.json','./icon-192.png','./icon-512.png'];
+const CACHE='filipinas-v8-4-0';
+const APP_SHELL=['./','./index.html','./style.css','./today.css','./data.js','./photos.js','./script.js','./today.js','./manifest.json','./icon-192.png','./icon-512.png'];
 const LOCAL_IMAGES=['./images/airport.webp','./images/bukidnon.webp','./images/camiguin.webp','./images/cdo.webp','./images/cebu.webp','./images/ferry.webp','./images/fort-san-pedro.webp','./images/guiob.webp','./images/iligan.webp','./images/kawasan.webp','./images/mantigue.webp','./images/oslob.webp','./images/santo-nino.webp','./images/sirao.webp','./images/taoist-temple.webp','./images/temple-of-leah.webp','./images/tuasan.webp'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll([...APP_SHELL,...LOCAL_IMAGES]))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
