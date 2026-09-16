9de083361c7fea8c5047e786863bc69da396f064
NachHR <120605029+NachHR@users.noreply.github.com>
2026-09-16 17:53:24 +0200

    release(v8.6.3): invalidate previous shell cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='filipinas-v8-6-2';
+const CACHE='filipinas-v8-6-3';
 const APP_SHELL=['./','./index.html','./style.css','./today.css','./budget.css','./data.js','./flights.js','./photos.js','./script.js','./today.js','./budget.js','./i18n.js','./v86.js','./v862.js','./manifest.json','./flag-ph.svg','./icon-192.png','./icon-512.png'];
 const LOCAL_IMAGES=['./images/airport.webp','./images/bukidnon.webp','./images/camiguin.webp','./images/cdo.webp','./images/cebu.webp','./images/ferry.webp','./images/fort-san-pedro.webp','./images/guiob.webp','./images/iligan.webp','./images/kawasan.webp','./images/mantigue.webp','./images/oslob.webp','./images/santo-nino.webp','./images/sirao.webp','./images/taoist-temple.webp','./images/temple-of-leah.webp','./images/tuasan.webp'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll([...APP_SHELL,...LOCAL_IMAGES])).then(()=>self.skipWaiting())));
