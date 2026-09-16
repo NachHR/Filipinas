4cd659033b78d11e735b14c6ebde55597c6c93bd
NachHR <120605029+NachHR@users.noreply.github.com>
2026-09-16 06:30:31 +0200

    feat: harden v8.3 offline cache strategy

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,13 @@
-const CACHE='filipinas-v8-2-1';
+const CACHE='filipinas-v8-3-0';
 const APP_SHELL=['./','./index.html','./style.css','./data.js','./photos.js','./script.js','./manifest.json','./icon-192.png','./icon-512.png'];
-const LOCAL_IMAGES=[
-'./images/airport.webp','./images/bukidnon.webp','./images/camiguin.webp','./images/cdo.webp','./images/cebu.webp','./images/ferry.webp','./images/fort-san-pedro.webp','./images/guiob.webp','./images/iligan.webp','./images/kawasan.webp','./images/mantigue.webp','./images/oslob.webp','./images/santo-nino.webp','./images/sirao.webp','./images/taoist-temple.webp','./images/temple-of-leah.webp','./images/tuasan.webp'
-];
+const LOCAL_IMAGES=['./images/airport.webp','./images/bukidnon.webp','./images/camiguin.webp','./images/cdo.webp','./images/cebu.webp','./images/ferry.webp','./images/fort-san-pedro.webp','./images/guiob.webp','./images/iligan.webp','./images/kawasan.webp','./images/mantigue.webp','./images/oslob.webp','./images/santo-nino.webp','./images/sirao.webp','./images/taoist-temple.webp','./images/temple-of-leah.webp','./images/tuasan.webp'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll([...APP_SHELL,...LOCAL_IMAGES])).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('message',event=>{if(event.data?.type==='SKIP_WAITING')self.skipWaiting()});
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET')return;
   event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
     if(response.ok){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));}
     return response;
-  }).catch(()=>caches.match('./index.html'))));
+  }).catch(()=>event.request.mode==='navigate'?caches.match('./index.html'):Response.error())));
 });
